1e34f6a1e2f3994b3326d2aaecada96a03accf78
Rodrigo Hausen <hausen@compscinet.org>
2015-02-16 21:27:25 -0200

    bug fix

--- util/latextosvg.sh
+++ util/latextosvg.sh
@@ -27,7 +27,7 @@ rm -f tmp.dvi
 latex -interaction=nonstopmode tmp.tex > /dev/null 2>&1
 if [ -f tmp.dvi ]
 then
-  dvisvgm -b0 -s tmp.dvi | sed -e 's/psyr/Times New Roman/g; s/ptmri8r/Times New Roman;font-style:italic/g; s/ptmr8r/Times New Roman/g; s/cmmi10/Times New Roman/g; s/cmsy10/Times New Roman/g; s/cmex10/cmr10/g; s/㐀/−/g; s/㐕/≥/g; s/㐔/≤/g; s/㐁/)/g;' 2> /dev/null
+  dvisvgm -b0 -s tmp.dvi | sed -e 's/font-family:psyro/font-family:Times New Roman;font-style:oblique/g; s/psyr/Times New Roman/g; s/ptmri8r/Times New Roman;font-style:italic/g; s/ptmr8r/Times New Roman/g; s/cmmi10/Times New Roman/g; s/cmsy10/Times New Roman/g; s/cmex10/cmr10/g; s/㐀/−/g; s/㐕/≥/g; s/㐔/≤/g; s/㐁/)/g;' 2> /dev/null
 else
   echo "ERROR running latex!"
   exit 1
