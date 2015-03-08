58fa72826bdbac17eb37ad0c8b13d132e46bc78d
Rodrigo Hausen <hausen@compscinet.org>
2015-03-07 21:49:46 -0300

    adicionando funcionalidade para criar svg só com curvas ao iniciar linha com {}

--- util/expressions.sh
+++ util/expressions.sh
@@ -6,5 +6,11 @@ while read -r line
 do
   let i=$i+1
   file=`printf "$prefix%03d.svg" $i`
-  echo "$line" | "$dir/latextosvg.sh" $* > $file
+  opts=""
+  echo "$line" | grep '^{' -q
+  if [ $? == 0 ]
+  then
+    opts="-n"
+  fi
+  echo "$line" | "$dir/latextosvg.sh" $opts $* > $file
 done
